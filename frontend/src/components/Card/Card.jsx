ad7c3fa7e2f4c8f1ab8c0c1e4165ab2afb037ea0
MarcosCastr0 <117954570+MarcosCastr0@users.noreply.github.com>
2022-12-20 21:50:43 -0300

    Final

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,17 +1,32 @@
 import "./card.css";
+import API from "../../api/api";
 
-function Card(){
-    return(
-        <div className="card mb-3" >
-          <img src="http://picsum.photos/300/200" className="card-img-top" alt="..." />
+function Card(props){
+
+  async function deleteCard(event){
+    const buttonEl = event.target 
+    
+    buttonEl.disabled = true;
+
+    const request = await API.item.delete(props.id)
+
+    console.log(request);
+
+    props.refreshItems();
+
+    buttonEl.disabled = false;
+  }
+  return(
+        <div className="card mb-3" style={{height:"100%"}}>
+          <img src={props.url} className="card-img-top" alt={props.name} />
           <div className="card-body">
             <a href="#" className="card-title">
-              <h5 className="card-title"> Card title </h5>
+              <h5 className="card-title">{props.name}</h5>
             </a>
-            <p className="card-text">Categoria</p>
-            <a href="#" className="btn btn-danger">
+            <p className="card-text">{props.category}</p>
+            <button onClick={deleteCard} className="btn btn-danger">
               Remover
-            </a>
+            </button>
           </div>
         </div>
     )
--- frontend/src/components/itemList/itemList.jsx
+++ frontend/src/components/itemList/itemList.jsx
@@ -1,21 +1,35 @@
-import Card from '../card/card'
+import Card from '../card/card';
+import { useState, useEffect } from 'react';
+import API from "../../api/api";
 
 function ItemList() {
+  const [items, setItems] = useState([]);
+
+  async function getItems(){
+    const itensDoBanco = await API.item.list()
+    const itensJSON = await itensDoBanco.json()
+    console.log(itensJSON)
+
+    setItems(itensJSON);
+  }
+
+  useEffect(function(){
+    getItems();
+  },[])
+
   return (
     <div className="content container mb-3 mt-3">
       <div className="row">
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <Card />
-        </div>
+        {items.map((el)=>(
+          <div className="col-sm-4 align-items-strech" key={el._id}>
+          <Card 
+            id={el._id}
+            name={el.name}
+            url={el.imageUrl}
+            category={el.category.name}
+            refreshItems={getItems} />
+          </div>
+        ))}
       </div>
     </div>
   )
